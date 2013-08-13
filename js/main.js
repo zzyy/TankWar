1d99b4ba370c499c01a1ded47865d93ac5ced16d
unknown <P0025898@ZP0025898.sz.vsg.com>
2013-08-13 17:25:40 +0800

    example

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@
 	var painter=document.getElementById('painter');
 	var ctx=painter.getContext('2d');
 	ctx.beginPath();
-	ctx.arc(100 , 100, 30, 0, 2*Math.PI);
+	ctx.arc(100 , 100, 10, 0, 2*Math.PI);
+	ctx.fill();
+	ctx.stroke();
+	
+	ctx.beginPath();
+	ctx.moveTo(100,30);
+	ctx.lineTo(100,100);
+	ctx.strokeStyle='blue';
+	ctx.lineWidth=3;
+	
+	ctx.strokeStyle='red';
+	ctx.lineWidth=1;
+	ctx.arcTo(100,150,150,150,50);
+	ctx.lineTo(150, 50);
 	ctx.stroke();
 })()
